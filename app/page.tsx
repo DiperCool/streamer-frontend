d81569b93a59e575fd8cc03b2796a106a43939e9
[dyad] <git@dyad.sh>
2025-09-10 14:36:14 +0300

    [dyad] Устранено лишнее пространство в списке топ-стримов - wrote 1 file(s)

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default function HomePage() {
           ) : (
             // Desktop layout
             <div className="px-4 py-8">
-              <div className="flex w-full"> {/* Removed h-[66vh] */}
+              <div className="flex w-full items-start"> {/* Added items-start here */}
                 {/* Left section: Main Stream Player with Overlay */}
                 <div className="flex-[4] relative bg-gray-800 rounded-lg overflow-hidden aspect-video">
                   {hasStreamSources ? (
@@ -147,7 +147,7 @@ export default function HomePage() {
                 </div>
 
                 {/* Right section: Vertical list of other top streams */}
-                <div className="flex-1 flex flex-col space-y-5 pl-5 h-full"> {/* Added h-full back */}
+                <div className="flex-1 flex flex-col space-y-5 pl-5"> {/* Removed h-full here */}
                   {topStreams.slice(1, 4).map((stream, index) => (
                     <VerticalStreamCard
                       key={stream.id}
